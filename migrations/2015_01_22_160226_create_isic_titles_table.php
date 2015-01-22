86207ec646461880b7854c042ec262104eed224e
Victor Pierre <victor.alves@designshopp.com>
2015-01-22 16:06:06 -0500

    adding migrations

---
+++ migrations/2015_01_22_160226_create_isic_titles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateIsicTitlesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('isic_titles', function(Blueprint $table)
+		{
+			$table->integer('id', true);
+			$table->integer('sort_order');
+			$table->string('code', 45)->nullable();
+			$table->text('description')->nullable();
+			$table->string('explanatory_note_inclusion', 500)->nullable();
+			$table->string('explanatory_note_exclusion', 500)->nullable();
+			$table->primary(['id','sort_order']);
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('isic_titles');
+	}
+
+}
---
+++ migrations/2015_01_22_160245_create_isic_structure_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateIsicStructureTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('isic_structure', function(Blueprint $table)
+		{
+			$table->integer('id', true);
+			$table->integer('sort_order');
+			$table->string('code', 45)->nullable();
+			$table->string('code_level', 45)->nullable();
+			$table->string('level_1', 45)->nullable();
+			$table->string('level_2', 45)->nullable();
+			$table->string('level_3', 45)->nullable();
+			$table->string('level_4', 45)->nullable();
+			$table->string('level_5', 45)->nullable();
+			$table->string('level_6', 45)->nullable();
+			$table->primary(['id','sort_order']);
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('isic_structure');
+	}
+
+}
