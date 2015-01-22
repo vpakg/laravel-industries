170d823792b4d1ebdc8bb156e5286918f60b3eca
Victor <vpakg@users.noreply.github.com>
2015-01-22 16:15:57 -0500

    Update 2015_01_22_160226_create_isic_titles_table.php

--- migrations/2015_01_22_160226_create_isic_titles_table.php
+++ migrations/2015_01_22_160226_create_isic_titles_table.php
@@ -21,6 +21,8 @@ class CreateIsicTitlesTable extends Migration {
 			$table->string('explanatory_note_inclusion', 500)->nullable();
 			$table->string('explanatory_note_exclusion', 500)->nullable();
 			$table->primary(['id','sort_order']);
+			$table->timestamps();
+
 		});
 	}
 
