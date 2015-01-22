40180124abd6e21507d68361f0fa0bcf0ba7e778
Victor Pierre <victor.alves@designshopp.com>
2015-01-22 15:51:53 -0500

    adding mysql inserts

---
+++ resources/sql_inserts/isic_level_names.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: SELECT * FROM isic.isic_level_names
+LIMIT 0, 1000
+
+-- Date: 2015-01-22 15:50
+*/
+INSERT INTO `isic_level_names` (`id`,`code_level`,`level_name`) VALUES (1,1,'Section');
+INSERT INTO `isic_level_names` (`id`,`code_level`,`level_name`) VALUES (2,2,'Division');
+INSERT INTO `isic_level_names` (`id`,`code_level`,`level_name`) VALUES (3,3,'Group');
+INSERT INTO `isic_level_names` (`id`,`code_level`,`level_name`) VALUES (4,4,'Class');
